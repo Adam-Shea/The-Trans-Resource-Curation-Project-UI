750ba6e4e68e46551a4f8cfbfdb1c21e8156d064
Adam Shea <davlyn.sheaa@outlook.com>
2022-05-16 23:51:44 +0100

    only generate gz

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,15 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 })
+module.exports = {
+  pluginOptions: {
+    compression: {
+      gzip: {
+        filename: '[file].gz[query]',
+        algorithm: 'gzip',
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        minRatio: 0.8,
+      }
+    }
+  }
+}
