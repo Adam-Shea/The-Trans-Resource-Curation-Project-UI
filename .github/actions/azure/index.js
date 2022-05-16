56e7e77ce4a449df2815138cbccd042bbb1624d1
Adam Shea <davlyn.sheaa@outlook.com>
2022-05-16 21:56:08 +0100

    test

--- .github/actions/azure/index.js
+++ .github/actions/azure/index.js
@@ -88,9 +88,9 @@ const DeployToAzureStorage = async () => {
                         core.info("includes js")
                     }
 
-                    exec.exec(azCopyCommand, ["copy", subPath, `${urlHost}${container}/${file}/${subFile}?${urlQuery}`, contentType])
+                    await exec.exec(azCopyCommand, ["copy", subPath, `${urlHost}${container}/${file}/${subFile}?${urlQuery}`, contentType])
                     if (contentEncoding) {
-                        exec.exec(azCopyCommand, ["copy", subPath, `${urlHost}${container}/${file}/${subFile}?${urlQuery}`, contentEncoding])
+                        await exec.exec(azCopyCommand, ["copy", subPath, `${urlHost}${container}/${file}/${subFile}?${urlQuery}`, contentEncoding])
                     }
                 }
             } else {
@@ -112,9 +112,9 @@ const DeployToAzureStorage = async () => {
                 } else if (path.includes(".gz")) {
                     contentEncoding = '--content-encoding=gzip'
                 }
-                exec.exec(azCopyCommand, ["copy", path, destUrl, contentType])
+                await exec.exec(azCopyCommand, ["copy", path, destUrl, contentType])
                 if (contentEncoding) {
-                    exec.exec(azCopyCommand, ["copy", path, destUrl, contentEncoding])
+                    await exec.exec(azCopyCommand, ["copy", path, destUrl, contentEncoding])
                 }
             }
         }
